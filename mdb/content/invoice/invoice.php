c01794ecd49f97658b694cd6edac1395b8bdd160
pawel-lenovo <pawelgrzegorzewski7@gmail.com>
2020-03-28 19:59:11 +0100

    logout path corrected

--- mdb/content/invoice/invoice.php
+++ mdb/content/invoice/invoice.php
@@ -48,7 +48,7 @@ $loggedUser = new User();
                         <h1 id="title"><a href ="../../index.php"><img src="../../resources/instalplast.png" style="width:300px;"></h1>
                     </td>
                     <td style = "text-align:right">
-                        <span>Zalogowany jako: <?php echo $_SESSION['user'] ?>&ensp;</span><a href = "../logout.php"><button class="btn btn-danger" value="">Wyloguj</button></a>
+                        <span>Zalogowany jako: <?php echo $_SESSION['user'] ?>&ensp;</span><a href = "../core/logout.php"><button class="btn btn-danger" value="">Wyloguj</button></a>
                         </button>
                     </td>
                 </tr>
