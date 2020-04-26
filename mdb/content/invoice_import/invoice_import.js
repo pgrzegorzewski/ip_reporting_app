6157aeff9134f812b6d3b824d0895b3792764f37
pawel-lenovo <pawelgrzegorzewski7@gmail.com>
2020-04-26 17:26:27 +0200

    filter code refactor

--- mdb/content/invoice_import/invoice_import.js
+++ mdb/content/invoice_import/invoice_import.js
@@ -3,97 +3,7 @@ var items = [];
 $(document).ready(function(){
     appendAddInvoice() ; //to be deleted
     $('#data-table').DataTable();
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type:'region'},
-        dataType: 'json',
-        success:function(response){
-            var len = response.length;
-            for( var i = 0; i<len; i++){
-                var region_id = response[i]['region_id'];
-                var region_name = response[i]['region_nazwa'];
-
-                $("#region").append("<option value='"+region_id+"'>"+region_name+"</option>");
-            }
-        }
-    });
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type: 'country'},
-        dataType: 'json',
-        success:function(response){
-            var len = response.length;
-            for( var i = 0; i < len; i++){
-                var country_id = response[i]['kraj_id'];
-                var country_name = response[i]['kraj_nazwa'];
-
-                $("#country").append("<option value='"+country_id+"'>"+country_name+"</option>");
-            }
-        }
-    });
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type:'voivodeship'},
-        dataType: 'json',
-        success:function(response){
-            var len = response.length;
-            for( var i = 0; i<len; i++){
-                var voivodeship_id = response[i]['wojewodztwo_id'];
-                var voivodeship_name = response[i]['wojewodztwo_nazwa'];
-
-                $("#voivodeship").append("<option value='"+voivodeship_id+"'>"+voivodeship_name+"</option>");
-            }
-        }
-    });
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type:'client'},
-        dataType: 'json',
-        success:function(response){
-            var len = response.length;
-            for( var i = 0; i<len; i++){
-                var client_id = response[i]['kontrahent_id'];
-                var client_name = response[i]['kontrahent_nazwa'];
-
-                $("#client").append("<option value='"+client_id+"'>"+client_name+"</option>");
-            }
-        }
-    });
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type:'salesman'},
-        dataType: 'json',
-        success:function(response){
-            var len = response.length;
-            for( var i = 0; i<len; i++){
-                var salesman_id = response[i]['uzytkownik_id'];
-                var salesman_name = response[i]['uzytkownik_nazwa'];
-
-                $("#salesman").append("<option value='"+salesman_id+"'>"+salesman_name+"</option>");
-            }
-        }
-    });
-
-    $.ajax({
-        url: "./invoice_import_filters.php",
-        type: 'post',
-        data: {type:'item'},
-        dataType: 'json',
-        success:function(response){
-            items = response;
-        }
-    });
-
+    importFilters();
 });
 
 $(document).ready(function(){
@@ -213,6 +123,116 @@ $(document).ready(function(){
 
 });
 
+function importRegionFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type:'region'},
+      dataType: 'json',
+      success:function(response){
+          var len = response.length;
+          for( var i = 0; i<len; i++){
+              var region_id = response[i]['region_id'];
+              var region_name = response[i]['region_nazwa'];
+
+              $("#region").append("<option value='"+region_id+"'>"+region_name+"</option>");
+          }
+      }
+  });
+}
+
+function importCountryFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type: 'country'},
+      dataType: 'json',
+      success:function(response){
+          var len = response.length;
+          for( var i = 0; i < len; i++){
+              var country_id = response[i]['kraj_id'];
+              var country_name = response[i]['kraj_nazwa'];
+
+              $("#country").append("<option value='"+country_id+"'>"+country_name+"</option>");
+          }
+      }
+  });
+}
+
+function importVoivodeshipFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type:'voivodeship'},
+      dataType: 'json',
+      success:function(response){
+          var len = response.length;
+          for( var i = 0; i<len; i++){
+              var voivodeship_id = response[i]['wojewodztwo_id'];
+              var voivodeship_name = response[i]['wojewodztwo_nazwa'];
+
+              $("#voivodeship").append("<option value='"+voivodeship_id+"'>"+voivodeship_name+"</option>");
+          }
+      }
+  });
+}
+
+function importClientFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type:'client'},
+      dataType: 'json',
+      success:function(response){
+          var len = response.length;
+          for( var i = 0; i<len; i++){
+              var client_id = response[i]['kontrahent_id'];
+              var client_name = response[i]['kontrahent_nazwa'];
+
+              $("#client").append("<option value='"+client_id+"'>"+client_name+"</option>");
+          }
+      }
+  });
+}
+
+function importSalesmanFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type:'salesman'},
+      dataType: 'json',
+      success:function(response){
+          var len = response.length;
+          for( var i = 0; i<len; i++){
+              var salesman_id = response[i]['uzytkownik_id'];
+              var salesman_name = response[i]['uzytkownik_nazwa'];
+
+              $("#salesman").append("<option value='"+salesman_id+"'>"+salesman_name+"</option>");
+          }
+      }
+  });
+}
+
+function importItemFilter() {
+  $.ajax({
+      url: "./invoice_import_filters.php",
+      type: 'post',
+      data: {type:'item'},
+      dataType: 'json',
+      success:function(response){
+          items = response;
+      }
+  });
+}
+
+function importFilters() {
+  importRegionFilter();
+  importCountryFilter();
+  importVoivodeshipFilter();
+  importClientFilter();
+  importSalesmanFilter();
+  importItemFilter();
+}
 
 function higlightEmptyItem() {
   $("select.item").change(function(){
