af9f92bb3fa1fbd93870a8664952083e78d342f2
pawel-lenovo <pawelgrzegorzewski7@gmail.com>
2020-05-16 20:53:23 +0200

    handling csv with ;

--- mdb/content/invoice_import/import_csv.php
+++ mdb/content/invoice_import/import_csv.php
@@ -1,7 +1,6 @@
 <?php
 
 if(!empty($_FILES['csv_file']['name'])) {
-
     $file_name = $_FILES['csv_file']['name'];
     $uploaddir = $_SERVER["DOCUMENT_ROOT"]. '/ip_reporting_app/uploads/';
     $uploadfile = $uploaddir . basename($_FILES['csv_file']['name']);
@@ -10,15 +9,17 @@ if(!empty($_FILES['csv_file']['name'])) {
         $file_data = fopen($uploadfile, 'r');
         fgetcsv($file_data);
 
-        while($row = fgetcsv($file_data)) {
+        while($row = fgetcsv($file_data, 0, ";")) {
+          if(intVal($row[0])) {
             $data[] = array(
                 'lp' => intVal($row[0]),
                 'towar' => $row[1],
                 'ilosc' => intVal($row[2]),
                 'jm' => $row[3],
-                'cena' => $row[4],
+                'cena' => (float)str_replace(',', '.', $row[4]),
                 'edytuj' => "<button type='button' class='table-remove btn btn-danger btn-rounded btn-sm my-0 waves-effect waves-light'>Usuń</button>"
             );
+          }
         }
         echo json_encode($data);
     } else {
