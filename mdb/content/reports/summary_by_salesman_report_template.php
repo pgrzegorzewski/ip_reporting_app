f076ee54f7eb4d6e31f2725cb32834f265796997
pawel-lenovo <pawelgrzegorzewski7@gmail.com>
2020-08-15 12:33:09 +0200

    button for late pay table added

--- mdb/content/reports/summary_by_salesman_report_template.php
+++ mdb/content/reports/summary_by_salesman_report_template.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  require '../core/connect.php';
+?>
+
 <div class="form-row">
   <div class="col-md-3">
     <div class="md-form form-group">
@@ -12,13 +17,30 @@
     </div>
   </div>
   <div class="col-md-1">
-    <button class="btn btn-info" id = "salesman_summary_data_refresh"><span id="salesman_summary_data_refresh_span">Odśwież/załaduj</span></button>
+    <div class="md-form form-group">
+      <button class="btn btn-info" id = "salesman_summary_data_refresh"><span id="salesman_summary_data_refresh_span">Odśwież/załaduj</span></button>
+    </div>
   </div>
 </div>
 <div style = "text-align:center">
     <span id="error_msg" style="color:red"></span>
     <br><br>
 </div>
+<?php
+  $query = "
+         SELECT * FROM  usr.sf_sprawdz_prawo_dostepu($1, 9)
+  ";
+  $hasAccessQuery = @pg_query_params($connection, $query, array($_SESSION['user']));
+  $hasAccess = pg_fetch_assoc($hasAccessQuery);
+  if($hasAccess['sf_sprawdz_prawo_dostepu'] == 1) {
+      echo "
+              <div style='text-align:center'>
+                <button class='btn btn-info' id = 'late_pay_table'><span id='late_pay_table_span'>Pokaż kwoty przeterm.</span></button>
+              </div>
+              <div id='late_pay_datatable' hidden=true></div>
+           ";
+  }
+?>
 <div class="table-responsive">
     <table class="table table-striped table-bordered" id="data-table">
         <thead>
